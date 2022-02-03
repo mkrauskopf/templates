c40e43417dcdff6019d4ef2995ea6d2682a3ecc0
Martin Krauskopf <martin.krauskopf@gmail.com>
2022-02-03 17:32:24 +0100

    Template for JavaScript + React project (no TS)

---
+++ javascript-react/src/Application.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Application = () => <div>Hello from React 🎉</div>
+
+export default Application
---
+++ javascript-react/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+}
